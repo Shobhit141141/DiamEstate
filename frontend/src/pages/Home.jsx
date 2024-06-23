d1c6dad3d71aa96a67688609e465b6de4b8e3154
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-06-23 21:35:21 +0530

    chore: initialization

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+function Home() {
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] flex items-center justify-center gap-5">
+        <p>Its a daisy Ui component</p>
+        <span className="loading loading-ring loading-lg"></span>
+      </div>
+    </>
+  );
+}
+
+export default Home;
