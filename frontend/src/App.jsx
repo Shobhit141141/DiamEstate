c2f2675a8d64fd933ff87d4ed56c11deac31dbac
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-06-28 23:58:14 +0530

    filter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-function App() {
+import Prodcuts from "./pages/Prodcuts";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Prodcuts />} />
         </Routes>
       </BrowserRouter>
     </>
