6d6c1df498243ae715baca332f52ab92cbdd827f
anshd15 <anshdoshi.d16@gmail.com>
2024-07-08 21:45:26 +0530

    bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import Prodcuts from './pages/Prodcuts';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DetailsPage from './pages/DetailsPage';
 
 import Navbar from './components/NavBar';
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<Navbar />
 				<div className='mt-[80px] main'>
 					<Routes>
+						<Route path='/details' element={<DetailsPage/>}/>
 						<Route path='/' element={<Home />} />
 						<Route path='/products' element={<Prodcuts />} />
 					</Routes>
