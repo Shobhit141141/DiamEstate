f43d3943672219ab5aadc1506afbc49024f7367e
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-07-14 20:41:09 +0530

    User and Property data models

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-  createdAt: { type: Date, default: Date.now() },
+	username: { type: String, required: true, unique: true },
+	wallet_address: { type: String, required: true, unique: true },
+	location: {
+		type: {
+      address: { type: String, required: true },
+			city: { type: String, required: true },
+			state: { type: String, required: true },
+			country: { type: String, required: true },
+		},
+		required: true,
+	},
+	my_listings: [
+		{ type: mongoose.Schema.Types.ObjectId, ref: 'Property', required: true },
+	],
+	my_transactions: [],
+	my_investments: [
+		{
+			type: { type: mongoose.Schema.Types.ObjectId, ref: 'Property', required: true },
+			share_per: { type: Number, required: true, default: 0 },
+		},
+	],
 });
 
 const User = mongoose.model('User', userSchema);
