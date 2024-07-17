618e0d07d6ed8ef879db0409fbb011aba1eb7e18
Aman Tiwari <amananjalitiwari2007@gmail.com>
2024-07-17 12:13:35 +0530

    transactions

--- frontend/src/apis/userApi.js
+++ frontend/src/apis/userApi.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { server_url } from '../config';
 import toast from 'react-hot-toast';
 
+const public_address = localStorage.getItem('public_address');
+
 export const listProperty = async (propertyData) => {
 	const response = await axios.post(
 		`${server_url}/user/list-property`,
@@ -69,5 +71,17 @@ export const handlePayment = async (amount, receiverPublicKey) => {
 	} catch (error) {
 		console.error(error);
 		toast.error('Payment Failed. Please try again.');
+	}
+};
+
+export const getDiamAccountTransactions = async () => {
+	try {
+		const resp = await axios.get(
+			`https://diamtestnet.diamcircle.io/accounts/${public_address}/transactions`
+		);
+		return resp.data;
+	} catch (error) {
+		console.log('Error fetching transactions: ', error);
+		toast.error('Error fetching transactions');
 	}
 };
--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,84 +1,95 @@
-import { useEffect, useState } from "react";
-import MyInvestment from "../components/MyInvestment";
-import Transaction from "../components/MyTransaction";
-import Listings from "../components/MyListings";
-import { getUserDetails } from "../apis/userApi";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import MyInvestment from '../components/MyInvestment';
+import Transaction from '../components/MyTransaction';
+import Listings from '../components/MyListings';
+import { getDiamAccountTransactions, getUserDetails } from '../apis/userApi';
+import { Link } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 function DashBoard() {
-  const [activeTab, setActiveTab] = useState("My Investments");
-  const [user, setUser] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
+	const [activeTab, setActiveTab] = useState('My Investments');
+	const [user, setUser] = useState([]);
+	const [transactions, setTransactions] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const handleTabClick = async (tab) => {
+		setActiveTab(tab);
+		toast.promise(
+			getDiamAccountTransactions().then((transactions) =>
+				setTransactions(transactions._embedded.records)
+			),
+			{
+				loading: 'Fetching Transactions from chain...',
+				success: 'Transactions fetched',
+				error: 'Failed to load Transactions',
+			}
+		);
+	};
 
-  useEffect(() => {
-    const getUser = async () => {
-      try {
-        setLoading(true);
-        const response = await getUserDetails();
-        setUser(response.data.result);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching user details:", error);
-        setLoading(false);
-      }
-    };
-
-    getUser();
-  }, []);
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-  return (
-    <div className="mt-[90px] p-6">
-      <h1 className="text-2xl font-bold mb-4 text-[#7065F0]">
-        <p className="text-black">
-          Hello,{" "}
-          <span className="text-[#7065F0]">{user.username || "Guest"}</span>
-          {user?.location
-            ? ` from ${user.location.city}, ${user.location.country}`
-            : ""}
-        </p>
-        {/* Welcome to Dashboard , Keep the track of your Estate */}
-      </h1>
-      <div className="flex space-x-4 border-b">
-        <button
-          className={`py-2 px-4 ${
-            activeTab === "My Investments"
-              ? "bg-[#7065F0] text-white rounded-[6px]"
-              : "bg-[#9c9c9c] text-white rounded-[6px]"
-          }`}
-          onClick={() => handleTabClick("My Investments")}
-        >
-          My Investments
-        </button>
-        <button
-          className={`py-2 px-4 ${
-            activeTab === "My Listings"
-              ? "bg-[#7065F0] text-white rounded-[6px]"
-              : "bg-[#9c9c9c] text-white rounded-[6px]"
-          }`}
-          onClick={() => handleTabClick("My Listings")}
-        >
-          My Listings
-        </button>
-        <button
-          className={`py-2 px-4 ${
-            activeTab === "My Transaction"
-              ? "bg-[#7065F0] text-white rounded-[6px]"
-              : "bg-[#9c9c9c] text-white rounded-[6px]"
-          }`}
-          onClick={() => handleTabClick("My Transaction")}
-        >
-          My Transactions
-        </button>
-      </div>
-      <div className="mt-4">
-        {activeTab === "My Investments" && (
-          <div className="flex flex-col gap-2">
-            {/* <MyInvestment
+	useEffect(() => {
+		const getUser = async () => {
+			try {
+				setLoading(true);
+				const response = await getUserDetails();
+				setUser(response.data.result);
+				setLoading(false);
+			} catch (error) {
+				console.error('Error fetching user details:', error);
+				setLoading(false);
+			}
+		};
+		getUser();
+	}, []);
+	if (loading) {
+		return <p>Loading...</p>;
+	}
+	return (
+		<div className='mt-[90px] p-6'>
+			<h1 className='text-2xl font-bold mb-4 text-[#7065F0]'>
+				<p className='text-black'>
+					Hello,{' '}
+					<span className='text-[#7065F0]'>{user.username || 'Guest'}</span>
+					{user?.location
+						? ` from ${user.location.city}, ${user.location.country}`
+						: ''}
+				</p>
+				{/* Welcome to Dashboard , Keep the track of your Estate */}
+			</h1>
+			<div className='flex space-x-4 border-b'>
+				<button
+					className={`py-2 px-4 ${
+						activeTab === 'My Investments'
+							? 'bg-[#7065F0] text-white rounded-[6px]'
+							: 'bg-[#9c9c9c] text-white rounded-[6px]'
+					}`}
+					onClick={() => handleTabClick('My Investments')}
+				>
+					My Investments
+				</button>
+				<button
+					className={`py-2 px-4 ${
+						activeTab === 'My Listings'
+							? 'bg-[#7065F0] text-white rounded-[6px]'
+							: 'bg-[#9c9c9c] text-white rounded-[6px]'
+					}`}
+					onClick={() => handleTabClick('My Listings')}
+				>
+					My Listings
+				</button>
+				<button
+					className={`py-2 px-4 ${
+						activeTab === 'My Transaction'
+							? 'bg-[#7065F0] text-white rounded-[6px]'
+							: 'bg-[#9c9c9c] text-white rounded-[6px]'
+					}`}
+					onClick={() => handleTabClick('My Transaction')}
+				>
+					My Transactions
+				</button>
+			</div>
+			<div className='mt-4'>
+				{activeTab === 'My Investments' && (
+					<div className='flex flex-col gap-2'>
+						{/* <MyInvestment
               src={
                 "https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?cs=srgb&dl=pexels-binyaminmellish-186077.jpg&fm=jpg"
               }
@@ -93,72 +104,110 @@ function DashBoard() {
               propertyName="La Villa"
               propertyAddress="223 Main St, Washington DC"
             /> */}
-            {user.my_investments.map((investment) => (
-              <Link to={`/details/${investment.property._id}`}>
-                <MyInvestment
-                  key={investment.property._id}
-                  src={investment.property.images[0]}
-                  propertyName={investment.property.title}
-                  propertyAddress={investment.property.location.address}
-                  share_per={investment.share_per}
-                  total_price={investment.property.total_price}
-                />
-              </Link>
-            ))}
-          </div>
-        )}
+						{user.my_investments.map((investment) => (
+							<Link to={`/details/${investment.property._id}`}>
+								<MyInvestment
+									key={investment.property._id}
+									src={investment.property.images[0]}
+									propertyName={investment.property.title}
+									propertyAddress={investment.property.location.address}
+									share_per={investment.share_per}
+									total_price={investment.property.total_price}
+								/>
+							</Link>
+						))}
+					</div>
+				)}
 
-        {activeTab === "My Listings" && (
-          <div className="flex flex-col gap-2">
-            {/* <Listings
-              src={
-                "https://www.adanirealty.com/-/media/Project/Realty/Blogs/Types-Of-Residential-Properties.png"
-              }
-              propertyName="Raj Residency"
-              propertyAddress="122 street , NYC"
-              percentageLeft={80}
-            /> */}
-            {user.my_listings.map((investment) => (
-              <Link to={`/details/${investment.property._id}`}>
-                <Listings
-                  key={investment.property._id}
-                  src={investment.property.images[0]}
-                  propertyName={investment.property.title}
-                  propertyAddress={investment.property.location.address}
-                  percentageLeft={investment.property.percentageLeft}
-                  total_price={investment.property.total_price}
-                />
-              </Link>
-            ))}
-          </div>
-        )}
-        {activeTab === "My Transaction" && (
-          <div className="flex flex-col gap-2">
-            <Transaction
-              src={
-                "https://www.tricityinfra.com/wp-content/uploads/2024/04/Real-estate.jpg"
-              }
-              propertyName="Home stay"
-              propertyAddress="123 Main St, Springfield"
-              amount="100,000"
-              transactionType="credit"
-              tokens="500"
-            />
-            <Transaction
-              src={
-                "https://www.fausettlaw.com/wp-content/uploads/real-estate-vs.-real-property-1200x630.jpg"
-              }
-              propertyName="Luxury Apt."
-              propertyAddress="456 Elm St, Shelbyville"
-              amount="50,000"
-              transactionType="debit"
-              tokens="250"
-            />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				{activeTab === 'My Listings' && (
+					<div className='flex flex-col gap-2'>
+						{user.my_listings.map((investment) => (
+							<Link to={`/details/${investment.property._id}`}>
+								<Listings
+									key={investment.property._id}
+									src={investment.property.images[0]}
+									propertyName={investment.property.title}
+									propertyAddress={investment.property.location.address}
+									percentageLeft={investment.property.percentageLeft}
+									total_price={investment.property.total_price}
+								/>
+							</Link>
+						))}
+					</div>
+				)}
+				{activeTab === 'My Transaction' && transactions.length ? (
+					<>
+						<p className='text-gray-600'>Transaction list for your account fetched from Diamante chain.</p>
+						<table className='min-w-full rounded-xl text-black border-2 border-black'>
+							<thead>
+								<tr className='border-b-2 border-black'>
+									<th className='border-b-2 border-black p-2 text-left'>
+										Date
+									</th>
+									<th className='border-b-2 border-black p-2 text-left'>
+										Hash
+									</th>
+									<th className='border-b-2 border-black p-2 text-left'>
+										Source Account
+									</th>
+									<th className='border-b-2 border-black p-2 text-left'>Fee</th>
+									<th className='border-b-2 border-black p-2 text-left'>
+										Success
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								{transactions.map((item, indx) => (
+									<tr
+										className={`border-t border-black ${
+											indx % 2 == 0 ? 'bg-indigo-300' : 'bg-white'
+										}`}
+									>
+										<td className='border-t border-black p-2'>
+											{new Date(item.created_at).toLocaleDateString()}
+										</td>
+										<td
+											onClick={() => {
+												navigator.clipboard.write(item.hash);
+												toast.success('Copied to clipboard!');
+											}}
+											className='border-t border-black p-2 cursor-pointer'
+										>
+											{item.hash.slice(0, 7) +
+												'...' +
+												item.hash.slice(
+													item.hash.length - 7,
+													item.hash.length - 1
+												)}
+										</td>
+										<td
+											onClick={() => {
+												navigator.clipboard.write(item.source_account);
+												toast.success('Copied to clipboard!');
+											}}
+											className='border-t border-black p-2 cursor-pointer'
+										>
+											{item.source_account}
+										</td>
+										<td className='border-t border-black p-2'>
+											{item.fee_charged}
+										</td>
+										<td className='border-t border-black p-2'>
+											{item.successful ? '✅' : '❌'}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</>
+				) : (
+					<p className='text-black text-center text-xl mx-auto my-16'>
+						No Transactions found!
+					</p>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default DashBoard;
